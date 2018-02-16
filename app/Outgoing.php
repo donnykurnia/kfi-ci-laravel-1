6ec6219f1647328d7b802a13d4d3ab68e95ca632
Donny Kurnia <donnykurnia@gmail.com>
2018-02-16 10:31:44 +0700

    add fillable

--- app/Outgoing.php
+++ app/Outgoing.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Outgoing extends Model
 {
+    protected $fillable = ['product_name', 'qty'];
+
     public function stock()
     {
         return $this->belongsTo('App\Stock');
