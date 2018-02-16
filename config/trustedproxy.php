188da0144a04fb26c5cd3e69536fa4db60d4a0b5
Donny Kurnia <donnykurnia@gmail.com>
2018-02-16 08:37:42 +0700

    fix constants

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -44,11 +44,11 @@ return [
     // https://devcenter.heroku.com/articles/getting-started-with-laravel#trusting-the-load-balancer
     'proxies' => '*',
     'headers' => [
-        Illuminate\Http\Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
-        Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
-        Illuminate\Http\Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
-        Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
-        Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
+        Illuminate\Http\Request::HEADER_FORWARDED         => null, // not set on AWS or Heroku
+        Illuminate\Http\Request::HEADER_X_FORWARDED_FOR   => 'X_FORWARDED_FOR',
+        Illuminate\Http\Request::HEADER_X_FORWARDED_HOST  => null, // not set on AWS or Heroku
+        Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+        Illuminate\Http\Request::HEADER_X_FORWARDED_PORT  => 'X_FORWARDED_PORT',
     ]
     
 ];
